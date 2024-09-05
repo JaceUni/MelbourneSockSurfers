dd8b2d6ad088d1e694cf48df30a92b3c4a1c5ea6
JaceUni <uni@jacebook.com.au>
2024-09-05 18:20:32 +1000

    Set up testing with App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,43 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Header from './components/header/header';        // An item to test
+import Navbar from './components/navigation/navbar';    // An item to test
+import Weather from './components/weather/weather';     // An item to test
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Test 1
+describe('Header', () => {
+  test('Header is rendered', () => {
+    render(<Header />);
+    const linkElement = screen.getByText(/Home of Melbourne's\s*Best Sliders/i);
+
+
+    // explicit assertion
+    expect(linkElement).toBeInTheDocument();
+  });
+});
+
+// Test 2
+describe('Navigation', () => {
+  test('renders navigation items', () => {
+    const { getByText } = render(<Navbar />);
+
+    // Verify that all navigation items are rendered
+    expect(getByText('Home')).toBeInTheDocument();
+    expect(getByText('Results')).toBeInTheDocument();
+    expect(getByText('Events')).toBeInTheDocument();
+    expect(getByText('Gallery')).toBeInTheDocument();
+    expect(getByText('Video')).toBeInTheDocument();
+    expect(getByText('Newsletter')).toBeInTheDocument();
+    expect(getByText('Contact')).toBeInTheDocument();
+  });
+});
+
+// Test 3
+describe('Weather', () => {
+  test('Weather section present', () => {
+    const { getByText } = render(<Weather />);
+
+    // Verify all are rendered
+    expect(getByText('Loading weather data...')).toBeInTheDocument();
+  });
 });
